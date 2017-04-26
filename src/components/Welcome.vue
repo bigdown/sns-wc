7b98d22ee66a19ceb83a00fca85b188b67c3d9b9
tanshen519 <tanshen519@13322.com>
2017-04-26 16:05:09 +0800

    init

---
+++ src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="welcome">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'welcome',
+  data () {
+    return {
+      msg: 'Welcome'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
